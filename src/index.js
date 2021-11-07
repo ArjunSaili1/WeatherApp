10660f36a56382565fa64a4c32fec5da6af2ac47
Arjun Saili <arjun7.saili@gmail.com>
2021-11-07 00:42:25 -0400

    Created main interface and implemented data display

--- src/index.js
+++ src/index.js
@@ -13,12 +13,12 @@ const displayControl = (()=>{
         if (inputField.value){
             const weatherObj = await apiLogic.searchAPI(inputField.value);
             if(weatherObj){
-                createDisplay(weatherObj);
+                createDisplay(weatherObj, "celsius");
             }
         }
     }
 
-    function createDisplay(weatherObj){
+    function createDisplay(weatherObj, unit){
         console.log(weatherObj);
         locationForm.style.display = 'none';
         const weatherContainer = document.querySelector(".weather-container");
@@ -26,21 +26,21 @@ const displayControl = (()=>{
         const locationText = document.querySelector(".location-text");
         locationText.textContent = weatherObj.name + ", " + weatherObj.country;
         const weatherStatus = document.querySelector(".weather-text");
-        weatherStatus.textContent = weatherObj.status + "|" + weatherObj.celsius.temp;
+        weatherStatus.textContent = weatherObj.status + "|" + weatherObj.unit.temp;
         const tempSymbol = document.createElement("sup");
         tempSymbol.classList.add("temp-unit");
         tempSymbol.textContent = "°C";
         weatherStatus.appendChild(tempSymbol);
         const feelsLike = document.querySelector(".feels-like");
-        feelsLike.textContent = weatherObj.celsius.feels_like;
+        feelsLike.textContent = weatherObj.unit.feels_like;
         feelsLike.appendChild(tempSymbol.cloneNode(true));
         const pressure = document.querySelector(".pressure");
-        pressure.textContent = weatherObj.celsius.pressure + " hPa";
+        pressure.textContent = weatherObj.unit.pressure + " hPa";
         const maxTemp = document.querySelector(".max-temp");
-        maxTemp.textContent = weatherObj.celsius.temp_max;
+        maxTemp.textContent = weatherObj.unit.temp_max;
         maxTemp.appendChild(tempSymbol.cloneNode(true));
         const minTemp = document.querySelector(".min-temp");
-        minTemp.textContent = weatherObj.celsius.temp_min;
+        minTemp.textContent = weatherObj.unit.temp_min;
         minTemp.appendChild(tempSymbol.cloneNode(true));
     }
     return{ initApp }    
